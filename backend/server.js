1287831c3726c281346911ba278d8b57eceb2e04
Zahid2297 <techpirate97@gmail.com>
2026-03-17 21:21:29 +0530

    Connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-import app from "./src/app.js";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./src/config/db.js";
+import app from "./src/app.js";
+
+connectDB();
 
 const port = process.env.PORT || 5000;
 

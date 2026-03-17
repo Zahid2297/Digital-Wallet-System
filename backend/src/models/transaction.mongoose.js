8040f169c4b6bab12c60dd86a1b76d0a26c79a3a
Zahid2297 <techpirate97@gmail.com>
2026-03-17 06:30:33 +0530

    add models and initialize auth

---
+++ backend/src/models/transaction.mongoose.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["credit", "debit"],
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "success", "failed"],
+      default: "success",
+    },
+  },
+  { timestamp: true },
+);
+
+export const transaction = mongoose.model("transaction", transactionSchema);

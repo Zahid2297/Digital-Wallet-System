f68ebffe382563c7f69e6d435250192611e13659
Zahid2297 <techpirate97@gmail.com>
2026-05-19 00:20:42 +0530

    add route

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.mongoose";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { asyncHandle, asyncHandler } from "../utils/asyncHandler";
 
@@ -29,5 +30,48 @@ export const register = asyncHandler(async (req, res) => {
   res.status(201).json({
     message: "USer register successfully",
     user,
+  });
+});
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  //email and password fill kiya ye nhi
+  if (!email || !password) {
+    throw new Error("All fields are required");
+  }
+
+  //  user find krte time we need password so we use -> .select("+password")
+  //    (because in schema password has select: false)
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+
+  //  Compare password
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid email or password");
+  }
+
+  // Generate token
+  const token = generateToken(user._id);
+
+  // Respond
+  res.status(200).json({
+    message: "Login successful",
+    token,
+    user: {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      walletBalance: user.walletBalance,
+    },
   });
 });
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { register } from "../controllers/auth.controllers.js";
-import { asyncHandler } from "../utils/asyncHandler";
+import { register, login } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;

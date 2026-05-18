a85fa5fe96723c8d84ad4310bed27cb9376ac9e2
Zahid2297 <techpirate97@gmail.com>
2026-05-19 01:46:37 +0530

    update middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.mongoose.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-export const protect = asyncHandler((req, res, next) => {
+export const protect = asyncHandler(async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     throw new Error("Not authorized & no token");
   }
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = User.findById(decoded.id).select("-password");
+  req.user = await User.findById(decoded.id).select("-password");
 
   next();
 });
